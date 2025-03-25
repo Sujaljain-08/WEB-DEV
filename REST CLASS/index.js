d69f6fd120d54ffb0cb7ecf3ef0673b9bc7aaf0f
Sujaljain-08 <sujalb.is24@rvce.edu.in>
2025-03-25 17:53:03 +0530

    post req added

--- REST CLASS/index.js
+++ REST CLASS/index.js
@@ -7,6 +7,7 @@ app.set("views",path.join(__dirname,"/views"));
 
 app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log("Server started");
@@ -15,7 +16,16 @@ app.listen(3000,()=>{
 let arr=[{username:"Sujal",content:"The second best time to start is now!!"}
     ,{username:"Sujal",content:"Batman has no limts!!"},
     {username:"Sujal",content:"WIN HEY YAA TOH LUN HEY!!"}];
+
 app.get("/posts",(req,res)=>{
     res.render("Quora.ejs",{arr});});
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+app.post("/posts",(req,res)=>{
+   let {username,content}=req.body;
+   arr.push({username,content});});
 
 
