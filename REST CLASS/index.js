e0942e6169bbf19fccef5071f8b0abcf7286f9a8
Sujaljain-08 <sujalb.is24@rvce.edu.in>
2025-03-25 18:53:21 +0530

    View in detail option added

--- REST CLASS/index.js
+++ REST CLASS/index.js
@@ -1,6 +1,7 @@
 let express=require("express");
 const app=express();
 let path=require("path");
+const { v4: uuidv4 } = require('uuid');
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
@@ -13,9 +14,9 @@ app.listen(3000,()=>{
     console.log("Server started");
 });
 
-let arr=[{username:"Sujal",content:"The second best time to start is now!!"}
-    ,{username:"Sujal",content:"Batman has no limts!!"},
-    {username:"Sujal",content:"WIN HEY YAA TOH LUN HEY!!"}];
+let arr=[{username:"Sujal",content:"The second best time to start is now!!",id:uuidv4()}
+    ,{username:"Sujal",content:"Batman has no limts!!",id:uuidv4()},
+    {username:"Sujal",content:"WIN HEY YAA TOH LUN HEY!!",id:uuidv4()}];
 
 app.get("/posts",(req,res)=>{
     res.render("Quora.ejs",{arr});});
@@ -26,6 +27,14 @@ app.get("/posts/new",(req,res)=>{
 
 app.post("/posts",(req,res)=>{
    let {username,content}=req.body;
-   arr.push({username,content});});
+   id=uuidv4();
+   arr.push({username,content,id});
+});
 
+app.get("/posts/:id",(req,res)=>{
+   let {id}=req.params;
+   let post=arr.find((p)=>id===p.id);
+   console.log(post);
+   res.render("explore.ejs",{post});
+});
 
