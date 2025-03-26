0fc9d242f9d9bb35a109d04445de95d035ea8373
Sujaljain-08 <sujalb.is24@rvce.edu.in>
2025-03-26 17:59:27 +0530

    functionalities added

--- DOM mini-project(simon game)/appsimon.js
+++ DOM mini-project(simon game)/appsimon.js
@@ -1,22 +1,58 @@
-let start=true;
-let arr=[];
+let start=false;
+let game=[];
+let user=[];
+let level=0;
+let idx=0;
 let btns=document.querySelectorAll(".box");
-for(let btn of btns){
-    btn.addEventListener("click",()=> {  console.log(btn.id + " clicked");setTimeout(()=>{btn.classList.remove("userflash")},450);btn.classList.add("userflash")})
-}
-function levelup(){
-    let Val=Math.floor(Math.random()*4);
-    arr.push(`btn${Val}`);
-    let flash=document.querySelector(`#btn${Val}`);
+
+function buttonflash(Val){
+    console.log("buttonflash called");
+    let btn=document.querySelector(`#btn${Val}`);
+    btn.classList.add("flashbtn");
     setTimeout(()=>{
-        flash.classList.remove("flashbtn")},450);
-    flash.classList.add("flashbtn");
+        btn.classList.remove("flashbtn")},500);
 }
 
-while(start){
-    let level=1;
-    levelup();
-    for(let i=0;i<level;i++){
-        
-    }
+function levelup(){   
+    level++; 
+    idx =0;
+    user=[];
+    let h3=document.querySelector("h3");
+    h3.innerText=`level ${level}`;
+    let Val=Math.floor(Math.random()*4);    
+    game.push(`btn${Val}`);
+    buttonflash(Val);
 }
+
+for(let btn of btns){
+    btn.addEventListener("click",()=>{
+        btn.classList.add("userflash");
+        setTimeout(()=>{btn.classList.remove("userflash")},350)
+        user.push(btn.id);
+        console.log(`user:${user}`);
+        console.log(`game:${game}`); 
+      if(user[idx]==game[idx]){
+        console.log(level);
+        console.log(idx);
+        idx++;
+        if(idx==level){
+            setTimeout(levelup,350);
+        }
+      }
+      else{
+        start=false;
+        let h3=document.querySelector("h3");
+        h3.innerText=`WRONG ANSWER!!   YOUR SCORE:${level},`;
+        level=0;
+        game=[];
+      }
+    });
+}
+
+document.addEventListener("keypress",()=>{   //starts game
+    if(start!=true){
+        start=true;
+       console.log("Game started");
+       levelup();
+}
+});
