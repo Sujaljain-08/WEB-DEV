9a7178782f7faa5a5830750086aaf3533696ce07
Sujaljain-08 <sujalb.is24@rvce.edu.in>
2025-04-29 16:13:38 +0530

    building basic react-based todo model

--- Basic-react-project/src/App.jsx
+++ Basic-react-project/src/App.jsx
@@ -1,20 +1,44 @@
 import { useState } from 'react';
-import './App.css';
+import './app.css';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [isTrue, setIsTrue] = useState(false);
-  const [count, setCount] = useState(0);
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-  function jabClick() {
-    setCount(count + 1);
-    setIsTrue(!isTrue);
+  function inputHandler(e) {
+    setNewTask(e.target.value);
+  }
+
+  function submitHandler() {
+    if (newTask.trim() !== "") {
+      setTasks((prevTasks) => [...prevTasks, { Task: newTask, id: uuidv4() }]);
+      setNewTask(""); // clear input after submit
+    }
+  }
+
+  function DeleteHandler(e){
+    console.dir(e);
   }
 
   return (
     <>
-      <button onClick={jabClick}>
-        {count} {isTrue.toString()}
-      </button>
+      <input 
+        onChange={inputHandler} 
+        placeholder="Add a task" 
+        value={newTask} 
+      />
+      <br />
+      <br />
+      <button onClick={submitHandler}>ADD TASK</button>
+      <hr />
+      <h3>Tasks TODO</h3>
+      <ul>
+        {tasks.map(task => (<>
+                                <li  onClick={DeleteHandler} key={task.id}>{task.Task} &nbsp; <button>Delete</button></li>
+                            </>
+        ))}
+      </ul>
     </>
   );
 }
